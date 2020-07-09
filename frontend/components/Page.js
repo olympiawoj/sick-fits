255f10152c86f1ad94c7050bf297a242a58e17e2
olympiawoj <owojcik7@gmail.com>
2020-07-09 09:02:14 -0400

    Global Styling and Typography with Styled Components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import styled, {ThemeProvider, injectGlobal} from 'styled-components'
 import Header from "./Header"
 import Meta from "./Meta"
-import styled, {ThemeProvider, injectGlobal} from 'styled-components'
 
 // these are strings because this is not CSS like a styled component
 // this is an object that needs to have quotes around each of them
@@ -25,6 +25,34 @@ const Inner = styled.div`
     max-width: ${props => props.theme.maxWidth};
     margin: 0 auto;
     padding: 2rem;
+`
+
+injectGlobal`
+    @font-face{
+        font-family: 'radnika_next';
+        src: url('/static/radnikanext-medium-webfont.woff2')
+        format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    };
+    html {
+        box-sizing: border-box;
+        font-size: 10px;
+    }
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+    body {
+        padding: 0;
+        margin: 0;
+        font-size: 1.5rem; /* 15px - base 10*/
+        line-height: 2;
+        font-family: 'radnika_next'
+    };
+    a {
+        text-decoration: none;
+        color: ${theme.black}
+    }
 `
 
 class Page extends Component {
