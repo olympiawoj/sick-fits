0ce7123a880cef235b1d881e367a43861a044b68
olympiawoj <owojcik7@gmail.com>
2020-07-14 13:11:02 -0400

    Items Creation and Prisma Yoga Flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,12 +1,22 @@
 const Mutations = {
-    createDog(parent, args, ctx, info){
-        global.dogs = global.dogs || [];
-        // create a dog!
-        const newDog = {name: args.name}
-        global.dogs.push(newDog)
-        console.log(args);
-        return newDog
+    async createItem(parent, args, ctx, info){
+        // TODO: Check if they are logged in
+        const item = await ctx.db.mutation.createItem({
+            data: {
+               ...args
+            }
+        }, info)
+
+        return item;
     }
+    // createDog(parent, args, ctx, info){
+    //     global.dogs = global.dogs || [];
+    //     // create a dog!
+    //     const newDog = {name: args.name}
+    //     global.dogs.push(newDog)
+    //     console.log(args);
+    //     return newDog
+    // }
 }
 
 ;
